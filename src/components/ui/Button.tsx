5e64cd9ba047bcb1b2f82084da8ec1f816b8b487
NFT_Shared Services <jassimshamim@gmail.com>
2025-10-26 22:39:58 +0530

    removing all files for updation

--- src/components/ui/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-children: React.ReactNode;
-}
-
-export const Button: React.FC<Props> = ({ children, className = '', ...rest }) => (
-<button {...rest} className={`px-3 py-1 rounded ${className}`}>{children}</button>
-);
--- src/components/ui/Input.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ className = '', ...rest }) => (
-<input {...rest} className={`rounded-md px-3 py-2 border ${className}`} />
-);
